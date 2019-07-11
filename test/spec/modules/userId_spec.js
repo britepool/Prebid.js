55e84fd7da1cddc1bf3877434723cd586c0b73df
Chris Faherty <chris@britepool.com>
2019-07-11 13:06:57 -0400

    * Added custom getter test

--- test/spec/modules/userId_spec.js
+++ test/spec/modules/userId_spec.js
@@ -460,7 +460,11 @@ describe('User ID', function() {
     const api_key = '1111';
     const aaid = '4421ea96-34a9-45df-a4ea-3c41a48a18b1';
     const idfa = '2d1c4fac-5507-4e28-991c-ca544e992dba';
+    const pbid = '279c0161-5152-487f-809e-05d7f7e653fd';
     const url_override = 'https://override';
+    const getter_override = function(params) {
+      return Promise.resolve(JSON.stringify({ 'primaryPBID': pbid }));
+    };
 
     it('sends x-api-key in header and one identifier', () => {
       const { params, headers, url, errors } = britepoolIdSubmodule.createParams({ api_key, aaid });
@@ -484,7 +488,20 @@ describe('User ID', function() {
     it('test url override', () => {
       const { params, headers, url, errors } = britepoolIdSubmodule.createParams({ api_key, aaid, url: url_override });
       expect(url).to.equal(url_override);
+      // Making sure it did not become part of params
       expect(params.url).to.be.undefined;
     });
+
+    it('test getter override', done => {
+      const { params, headers, url, getter, errors } = britepoolIdSubmodule.createParams({ api_key, aaid, url: url_override, getter: getter_override });
+      expect(getter).to.equal(getter_override);
+      // Making sure it did not become part of params
+      expect(params.getter).to.be.undefined;
+      const getId = britepoolIdSubmodule.getId({ api_key, aaid, url: url_override, getter: getter_override });
+      getId(result => {
+        assert.deepEqual(result, { 'primaryPBID': pbid });
+        done();
+      });
+	  });
   });
 });
